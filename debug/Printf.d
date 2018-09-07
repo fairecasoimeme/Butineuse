334876676a2dd9aecde83c34c3f7953dc728b4ac
KiwiHC16 <moulinb@free.fr>
2018-09-07 13:09:05 +0200

    Realignement with Jerome-PS
    Apres avoir repris ma base de code, Jerome a fait un super boulot pour améliorer nettement le sniffer. Je ne vais pas essayer de porter ses changement, je repars sur ca version du 07/09/2018.

--- debug/Printf.d
+++
@@ -1,6 +0,0 @@
-debug/Printf.o: src/Printf.c inc/Printf.h \
- c:/NXP/bstudio_nxp/sdk/JN-SW-4163/Components/Common/Include/jendefs.h
-
-inc/Printf.h:
-
-c:/NXP/bstudio_nxp/sdk/JN-SW-4163/Components/Common/Include/jendefs.h:
--- debug/Queue.d
+++
@@ -1,7 +0,0 @@
-debug/Queue.o: src/Queue.c \
- c:/NXP/bstudio_nxp/sdk/JN-SW-4163/Components/Common/Include/jendefs.h \
- inc/Queue.h
-
-c:/NXP/bstudio_nxp/sdk/JN-SW-4163/Components/Common/Include/jendefs.h:
-
-inc/Queue.h:
